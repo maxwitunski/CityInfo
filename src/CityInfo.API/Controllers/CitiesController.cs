de838001c4a2e0faf856b004b547b32194decf1f
maxwitunski <michael.witunski@onbase.com>
2017-03-29 14:42:43 -0400

    Add project files.

---
+++ src/CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using CityInfo.API.Models;
+using CityInfo.API.Services;
+using System.Collections.Generic;
+using AutoMapper;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CitiesController : Controller
+    {
+        private ICityInfoRepository _cityInfoRepository;
+
+        public CitiesController(ICityInfoRepository cityInfoRepository)
+        {
+            _cityInfoRepository = cityInfoRepository;
+        }
+
+
+        [HttpGet()]
+        public IActionResult GetCities()
+        {
+            var cityEntities = _cityInfoRepository.GetCities();
+            var results = Mapper.Map<IEnumerable<CityWithoutPointsOfInterestDto>>(cityEntities);
+            return Ok(results);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id, bool includePointsOfInterest = false)
+        {
+            var city = _cityInfoRepository.GetCity(id, includePointsOfInterest);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            if (includePointsOfInterest)
+            {
+                var cityResult = Mapper.Map<CityDto>(city);
+                return Ok(cityResult);
+            }
+            var cityWithoutPointsOfInterestResult = Mapper.Map<CityWithoutPointsOfInterestDto>(city);
+            return Ok(cityWithoutPointsOfInterestResult);
+        }
+
+    }
+}
---
+++ src/CityInfo.API/Services/CloudMailService.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace CityInfo.API.Services
+{
+    public class CloudMailService : IMailService
+    {
+        private string _mailTo = "admin@mycompany.com";
+        private string _mailFrom = "noreply@mycompany.com";
+
+        public void Send(string subject, string message)
+        {
+            // Send mail - output to debug window.
+            Debug.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with CloudMailService.");
+            Debug.WriteLine($"Subject: {subject}");
+            Debug.WriteLine($"Message: {message}");
+        }
+    }
+}
